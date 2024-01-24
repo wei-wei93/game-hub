a11934425022a411217e2648ee0f9abb1c990be1
Wei Wei <wwei13240@gmail.com>
2024-01-24 16:45:22 +0000

    Display critic score

---
+++ src/components/CriticScoreTag.tsx
@@ -0,0 +1,15 @@
+import { Tag } from "@chakra-ui/react";
+
+interface Props {
+  score: number; 
+}
+
+const CriticScoreTag = ({ score }: Props) => {
+  let color = score > 85? 'green.200' : score > 60? 'yellow.200' : '';
+
+  return (
+    <Tag color={color} fontSize={16}>{score}</Tag>
+  )
+}
+
+export default CriticScoreTag
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,27 @@
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'; //importing an object will import its dependence libraries too 
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList"; //importing an object will import its dependence libraries too
+import CriticScoreTag from "./CriticScoreTag";
 
 interface Props {
-  game: Game; 
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image}/>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScoreTag score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
