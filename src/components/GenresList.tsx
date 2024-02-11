2484cd230457235ed5c9221e171068a60857c59a
Wei Wei <wwei13240@gmail.com>
2024-02-11 01:12:46 +0000

    Clean up the genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,32 +1,44 @@
-import { HStack, Image, Link, List } from "@chakra-ui/react";
+import { HStack, Heading, Image, Link, List } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import { imageCropper } from "../services/image-cropper";
 
 interface Props {
-  selectedGenre: Genre | null, 
-  onSelectGenre: (genre: Genre) => void, 
+  selectedGenre: Genre | null;
+  onSelectGenre: (genre: Genre) => void;
 }
 
 const GenresList = ({ selectedGenre, onSelectGenre }: Props) => {
-  const { data: genres, error } = useGenres();  //rename data to genres
+  const { data: genres, error } = useGenres(); //rename data to genres
 
   if (error) return null;
 
   return (
-    <List spacing="10px" marginTop="20px" marginLeft="10px">
-      {genres.map((genre) => (
-        <HStack key={genre.id}>
-          <Image
-            key={genre.image_background}
-            borderRadius={8}
-            objectFit="cover"
-            src={imageCropper(genre.image_background)}
-            boxSize="32px"
-          />
-          <Link as={genre.id === selectedGenre?.id ? 'b' : 'a'} onClick={() => onSelectGenre(genre)} fontSize='lg' key={genre.id}>{genre.name}</Link>
-        </HStack>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBlock='10px' marginLeft='10px'>
+        Genres
+      </Heading>
+      <List spacing="10px" marginTop="20px" marginLeft="10px">
+        {genres.map((genre) => (
+          <HStack key={genre.id}>
+            <Image
+              key={genre.image_background}
+              borderRadius={8}
+              objectFit="cover"
+              src={imageCropper(genre.image_background)}
+              boxSize="32px"
+            />
+            <Link
+              as={genre.id === selectedGenre?.id ? "b" : "a"}
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              key={genre.id}
+            >
+              {genre.name}
+            </Link>
+          </HStack>
+        ))}
+      </List>
+    </>
   );
 };
 
